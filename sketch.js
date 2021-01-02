7380d63c2b089860c59fa7f610b61e25d4771b27
priyakumari091 <priya.kumari091@atlassian.com>
2021-01-02 19:08:15 +0530

    Adding constrained

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg,platform, constrainedLog,chainC;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +37,10 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    constrainedLog = new Log(230,180,80,PI/2);
+
+    chainC = new Chain(bird.body,constrainedLog.body)
+
 }
 
 function draw(){
@@ -61,4 +66,8 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    constrainedLog.display();
+
+    chainC.display();
 }
